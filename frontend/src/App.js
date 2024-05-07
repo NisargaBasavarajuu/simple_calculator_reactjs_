d81d19e2ed9976b4fe7c58155e2865aa8db76d6b
NisargaBasavarajuu <145655608+NisargaBasavarajuu@users.noreply.github.com>
2024-05-07 14:37:55 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [expression, setExpression] = useState('');
   const [result, setResult] = useState('');
 
   const handleChange = (event) => {
@@ -12,7 +12,7 @@ function App() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch(`https://16.171.38.33:8000/api/calculate/?expression=${encodeURIComponent(expression)}`);
+      const response = await fetch(`http://localhost:8000/api/calculate/?expression=${encodeURIComponent(expression)}`);
       const data = await response.json();
       if (data.error) {
         setResult("Error: " + data.error);
@@ -23,24 +23,16 @@ function App() {
       setResult("Error: " + error.message);
     }
   };
- """
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://16.171.38.33/api/calculate/?expression=${encodeURIComponent(expression)}');
-      const data = await response.json();
-      setMessage(data.message);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-  """
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Calculator App Message</h1>
-        <button onClick={fetchData}>Get Response</button>
-        <p>{message}</p>
+        <h1>Simple Calculator</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={expression} onChange={handleChange} />
+          <button type="submit">Calculate</button>
+        </form>
+        <h2>Result: {result}</h2>
       </header>
     </div>
   );
