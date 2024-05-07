df4f0aa5b9fdd34fd4fe479f2818a1c02f4c03ed
NisargaBasavarajuu <145655608+NisargaBasavarajuu@users.noreply.github.com>
2024-05-07 15:53:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,7 @@
 import React, { useState } from 'react';
+
 function App() {
   const [message, setMessage] = useState('');
-
-  const fetchData = async () => {
-    try {
-     const response = await fetch(`https://16.171.38.33:8000/api/calculate/?expression=${encodeURIComponent(expression)}`);
-      const data = await response.json();
-      if (data.error) {
-        setResult("Error: " + data.error);
-      } else {
-        setResult(data.result);
-      }
-    } catch (error) {
-      setResult("Error: " + error.message);
-    }
-  };
 
   const fetchData = async () => {
     try {
